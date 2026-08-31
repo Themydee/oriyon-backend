163bcabebfed386b988ac99dfa468f93d016cab1
Themydee <nifetemiboy@gmail.com>
2026-08-31 23:04:59 +0100

    updates

--- services/api-gateway/src/index.ts
+++ services/api-gateway/src/index.ts
@@ -50,7 +50,7 @@ app.use(
     },
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization", "x-user-id"],
+    allowedHeaders: ["Content-Type", "Authorization", "Cache-Control", "Pragma", "x-user-id", "x-user-email", "x-user-role"],
   }),
 );
 app.use(morgan("combined"));
